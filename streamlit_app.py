f6bfa9e2705256434d60f5e4ccb1b6dbe196475c
sharathkoushik <77142757+sharathkoushik@users.noreply.github.com>
2023-08-08 14:54:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.header('My parents healthy diner')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
